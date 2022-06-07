b9da781566732d85dccd8d0049a7b6d748d7f4d9
Paweł Puzio <pawel.puzio@twoupdigital.com>
2022-06-07 23:24:30 +0200

    high score in game over modal

--- src/components/GameOverModal.tsx
+++ src/components/GameOverModal.tsx
@@ -1,12 +1,33 @@
-import { Component } from 'solid-js';
+import { Component, createSignal, onMount, Show } from 'solid-js';
 
 import clsx from 'clsx';
 import { AMOUNT_OF_TRIES } from '../utils/consts';
 import { useAppContext } from '../stores/AppContext';
 import { DifficultySelect } from './DifficultySelect';
 
+const [isHighScore, setIsHighScore] = createSignal(false);
+
 const GameOverModal: Component = () => {
   const store = useAppContext();
+
+  const [difficultyHighScore, setDifficultyHighScore] = createSignal('0');
+
+  onMount(() => {
+    const highScoreFromStorage = localStorage.getItem(
+      `high-score-${store.difficulty()}`
+    );
+
+    highScoreFromStorage && setDifficultyHighScore(highScoreFromStorage);
+
+    if (Number(highScoreFromStorage) < store.score()) {
+      setIsHighScore(true);
+      localStorage.setItem(
+        'high-score-' + store.difficulty(),
+        `${store.score()}`
+      );
+    }
+  });
+
   return (
     <main
       class={clsx(
@@ -33,12 +54,26 @@ const GameOverModal: Component = () => {
           'justify-center'
         )}
       >
-        <h1>Game over! </h1>
-        <h2>
-          Your score - {store.score()} / {AMOUNT_OF_TRIES}
+        <h1 class={clsx('font-bold')}>Game over! </h1>
+        <h2 class={clsx('mt-2', 'flex', 'gap-1')}>
+          <p>Your score:</p>
+          <p class={clsx('font-semibold')}>
+            {store.score()} / {AMOUNT_OF_TRIES}
+          </p>
         </h2>
+        <Show
+          fallback={() => (
+            <h3 class={clsx('p-4', 'flex')}>
+              High score for this level:
+              <p class={clsx('font-semibold')}>{difficultyHighScore()}</p>
+            </h3>
+          )}
+          when={isHighScore()}
+        >
+          <h3 class={'p-4'}>New high score achieved!</h3>
+        </Show>
         <p>Want to play again?</p>
-        <p>Choose your difficulty</p>
+        <p class={'pb-4'}>Choose your difficulty</p>
         <DifficultySelect />
       </section>
     </main>
